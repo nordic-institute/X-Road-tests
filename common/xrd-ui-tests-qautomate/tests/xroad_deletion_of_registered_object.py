b7261cb5f23436ebeaf7c5f35b7ccf0c89f290f9
Lasse Matikainen <lasse.matikainen@qautomate.fi>
2017-10-25 16:10:59 +0300

    PVAYLADEV-945: Deletion test case fixes

--- common/xrd-ui-tests-qautomate/tests/xroad_deletion_of_registered_object.py
+++ common/xrd-ui-tests-qautomate/tests/xroad_deletion_of_registered_object.py
@@ -116,7 +116,6 @@ class Xroad_deletion_of_registered_object(BaseTest):
                 * :func:`~webframework.extension.util.common_utils.CommonUtils.open_url`, *TESTDATA[u'ss1_url']['url']*
                 * :func:`~common_lib.common_lib.Common_lib.log_out`
         """
-        print("tearDown")
         stop_log_time = self.common_lib.get_log_utc_time()
         if not self.is_last_test_passed():
             _, copy_log = self.get_log_file_paths()
@@ -125,56 +124,33 @@ class Xroad_deletion_of_registered_object(BaseTest):
             self.common_lib_ssh.get_all_logs_from_server(u'cs_url')
             self.common_lib_ssh.find_exception_from_logs_and_save(self.start_log_time, stop_log_time, u'cs_url', copy_log)
 
-        error = ""
-
+        # Step Restore central server backup in central server if needed
         if self.restore_cs:
             print("tearDown_restore_cs")
-            try:
-                # Step Restore central server backup in central server
-                self.common_utils.open_url(TESTDATA[u'cs_url']['url'])
-                self.component_cs_sidebar.open_backup_restore_view()
-                self.component_cs_backup.restore_backup()
-
-                # Step Verify member found in members table in central server
-                self.component_cs_sidebar.open_members_view()
-                self.component_cs_members.verify_members_does_contain_member(u'member1_configuration', u'member_name')
-
-                # Step Verify client found in central server
-                self.component_cs_sidebar.open_security_servers_view()
-                self.component_cs_sec_servers.verify_servers_does_contain_server(u'member1_configuration', u'member_name')
-            except Exception as e:
-                error = "\n" + str(e)
-                print("tearDown_restore_cs" + "\n" + error)
-
-            # Step Log out from central server
             self.common_utils.open_url(TESTDATA[u'cs_url']['url'])
-            self.common_lib.log_out()
+            self.component_cs_sidebar.open_backup_restore_view()
+            self.component_cs_backup.restore_backup()
 
+        # Step Restore security server in security server if needed
         if self.restore_ss:
             print("tearDown_restore_ss")
-            try:
-                # Step Restore security server in security server
-                self.common_utils.open_url(TESTDATA[u'ss1_url']['url'])
-                self.component_ss_sidebar.open_backup_restore_view()
-                self.component_ss_backup.restore_backup()
-
-                # Step Verify security serve subsystem in security server
-                self.component_ss_sidebar.open_security_servers_client_view()
-                self.component_cs_sec_servers.verify_table_contains_subsystem(u'member1_configuration')
-            except Exception as e:
-                error = "\n" + str(e)
-                print("tearDown_restore_ss" + "\n" + error)
-
-            # Step Log out from security
             self.common_utils.open_url(TESTDATA[u'ss1_url']['url'])
+            self.component_ss_sidebar.open_backup_restore_view()
+            self.component_ss_backup.restore_backup()
+
+        # Step Log out from security if logged in
+        self.common_utils.open_url(TESTDATA[u'ss1_url']['url'])
+        if not self.ss_login.verify_is_login_page():
             self.common_lib.log_out()
-        print("END")
+
+        # Step Log out from central server if logged in
+        self.common_utils.open_url(TESTDATA[u'cs_url']['url'])
+        if not self.cs_login.verify_is_login_page():
+            self.common_lib.log_out()
 
         # Step Return server to defaults
         self.common_lib_ssh.delete_files_from_directory(u'cs_url', strings.backup_directory)
-
-        if error:
-            print(error)
+        self.common_lib_ssh.delete_files_from_directory(u'ss1_url', strings.backup_directory)
 
     def test_deletion_of_the_owner_of_ss_from_cs(self):
         """
